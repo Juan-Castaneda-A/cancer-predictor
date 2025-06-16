c4df0c615a1ea7b0c4d0452d3da9cdee5fa326fb
Juan Pablo Castañeda Ávila <juacastaneda@unal.edu.co>
2025-06-16 13:05:15 -0500

    update16

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 class Config:
     # URL para el frontend (útil para CORS si no usas CORS(app) sin restricciones)
-    FRONTEND_URL = "https://cancer-frontend.onrender.com/" # O la URL de tu frontend si usas Live Server u otro puerto
+    FRONTEND_URL = "https://cancer-frontend.onrender.com" # O la URL de tu frontend si usas Live Server u otro puerto
     # Puerto del backend
     FLASK_RUN_PORT = 5000
     # Desactivar modo debug en producción
