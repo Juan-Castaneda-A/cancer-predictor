06bfaf219ed6e1e04cc2c76b1056073fb599deef
Juan Pablo Castañeda Ávila <juacastaneda@unal.edu.co>
2025-06-16 09:18:23 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from backend.config import Config
-from routes.predict_routes import predict_bp
+from backend.routes.predict_routes import predict_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
